22f62c0f763e187f1b8ae59e63efc289bb9edf8a
Shreeram <shreeram1162004@gmail.com>
2025-01-24 15:31:33 +0530

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the build output folder is set to 'dist'
-  },
-  base: '/<repository-name>/', // Replace <repository-name> with your GitHub repo name
+  
+  base: '/portfolio/', // Replace <repository-name> with your GitHub repo name
 })
