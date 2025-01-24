506ca3a6d8b2ba9e3cb2b4513790183d768f8c29
Shreeram <shreeram1162004@gmail.com>
2025-01-24 15:20:37 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the build output folder is set to 'dist'
+  },
+  base: '/<repository-name>/', // Replace <repository-name> with your GitHub repo name
 })
